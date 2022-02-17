36e276630d253f76b0def2ef908d23e22f05c972
NanamiJin <78638680+NanamiJin@users.noreply.github.com>
2022-02-17 15:43:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,3 @@ with st.echo(code_location='below'):
     five_times_gacha_button = st.button('5回登用')
 
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
